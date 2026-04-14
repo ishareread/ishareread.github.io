291063d05537600006c3669562ddafbb80389879
xiejava <shygiant@126.com>
2026-04-14 20:59:12 +0800

    ci: 添加 ESA Pages 构建脚本

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+set -e
+
+# 下载并安装 Hugo
+HUGO_VERSION="0.140.2"
+if [ ! -f /tmp/hugo ]; then
+    wget -qO /tmp/hugo.tar.gz "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+    tar -xzf /tmp/hugo.tar.gz -C /tmp
+fi
+
+# 构建
+/tmp/hugo --minify --baseURL "https://www.ishareread.com/"
+
+echo "Build complete. Output in public/"
+ls -la public/ | head -10
